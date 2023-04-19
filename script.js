3575900680d897849887b6c299728eedd99387fd
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-04-19 20:31:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function() {
-    document.body.textContent = "DOm load success";
+    document.body.textContent = "DOM load success";
 });
